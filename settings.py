65de0251b24875d96fc78591aa4dfcc0ec8a1dcc
NixhadAxhmedov <104731086+NixhadAxhmedov@users.noreply.github.com>
2022-05-15 06:09:58 +0400

    Nexua_Game
    Nssjjsjsjsjehjeje

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 TOKEN = "5030510796:AAEUDLAlcAjbCMTORkR5LZzr9_QW-am5MBo"
-with open('words.txt', 'r', encoding='utf-8') as file:
+with on('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()

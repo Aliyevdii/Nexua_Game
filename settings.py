3f778f03572eaef1490de1b9820b0c3522578689
NixhadAxhmedov <104731086+NixhadAxhmedov@users.noreply.github.com>
2022-05-15 06:05:39 +0400

    Sbwh
    Ahhw

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5030510796:AAEUDLAlcAjbCMTORkR5LZzr9_QW-am5MBo"
+TOKEN = "5388934744:AAG1jQmDqNIgRD71fK7UH0VMkzhj9sJ2NpU"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
